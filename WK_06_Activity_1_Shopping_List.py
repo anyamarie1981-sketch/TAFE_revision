4f27ce4752e6c55a46ab8ba65b5d00dd8433a417
Anya Polmear <20166966@tafe.wa.edu.au>
2026-03-12 17:59:46 +0800

    update string functin file name and wk06 shopping list activity

---
+++ WK_06_Activity_1_Shopping_List.py
@@ -0,0 +1,31 @@
+# WK_06_Activity_1_Shopping_List
+
+# File: C:\Users\dandy\OneDrive\
+#   TAFE - Onedrive\TAFE_revision\WK_06_Activity_1_Shopping_List.py
+# Author: Anya Polmear
+# Copyright 2026: Anya Polmear 2026
+
+"""
+Instructions:
+Start by creating a list with some initial items.
+Add an Item to the List and prompt the user to add an item to the list.
+Prompt the user to remove an item from the list. Hint: Use a `while` loop
+to ensure the item exists in the list before removing it.
+Run the Program by combining all the steps into a single program and
+run it to see how it works.
+Include:
+print()
+.title()
+.append
+input()
+while
+if 
+not in
+else
+break
+
+Task: 
+Run the code to see the output and test the program by adding and
+removing different items to see how it behaves. Happy coding! 😊
+
+"""
